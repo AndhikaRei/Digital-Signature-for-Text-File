8b99a12f890508ca127681e16706e04f02b2fc06
reymyr <reyhanemyr51@gmail.com>
2021-11-15 22:15:37 +0700

    change message

--- src/signature.py
+++ src/signature.py
@@ -30,17 +30,17 @@ class Signature:
     return hash == decrypted
 
 def main():
-    message = "KEMBUR kmeme burikkkkkkk"
-    print("Message:", message)
+  message = "Test message 1 2 3"
+  print("Message:", message)
 
-    keys = RSA.generateKey()
-    print("Keys:", keys)
+  keys = RSA.generateKey()
+  print("Keys:", keys)
 
-    signature = Signature.sign(message, keys[1])
-    print("Signature:", signature)
+  signature = Signature.sign(message, keys[1])
+  print("Signature:", signature)
 
-    verify = Signature.verify(message, signature, keys[0])
-    print("Verify:", verify)
+  verify = Signature.verify(message, signature, keys[0])
+  print("Verify:", verify)
     
 if __name__ == "__main__":
-    main()
+  main()
